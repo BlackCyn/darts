c69805a3f9ff3fa6853276b1f34e6ee18aed99a2
BlackCyn <72068412+BlackCyn@users.noreply.github.com>
2020-12-27 08:49:38 +0300

    Add files via upload

---
+++ darts.py
@@ -0,0 +1,34 @@
+game=int(input("Введите количество игр которое хотите сыграть: "))
+for i in range(game):
+    N,K=map(int,input("Количество секторов и номер черной мишени: ").split())
+    max=-999
+    arr = list(map(int, input("Введите баллы за сектор: ").split()))
+    if len(arr)!=N:
+        print('Введеено неверное количество секторов ')
+        break
+    else:
+            arr[K] = min(arr)
+            if arr[K] > 0:
+                arr[K] = 0
+            for i in range(N):
+                sum = 0
+                for j in range(N):
+                    d = i + j
+                    if d > N - 1:
+                        d = d - N
+                    sum = sum + arr[d]
+                    if sum > max:
+                        max = sum
+            print("Максимальный рузельтат, который может получить игрок = ",max)
+else:
+            if (K == -1):
+                for i in range(N):
+                    sum = 0
+                    for j in range(N):
+                        d = i + j
+                        if d > N - 1:
+                            d = d - N
+                        sum = sum + arr[d]
+                        if sum > max:
+                            max = sum
+                print("Максимальный рузельтат, который может получить игрок = ",max)
